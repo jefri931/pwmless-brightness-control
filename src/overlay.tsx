5132648dc09df60db1d3d45cdc48ee2057db7dbe
SN-Dan <152906479+SN-Dan@users.noreply.github.com>
2025-07-19 17:44:28 +0300

    Update overlay.tsx

--- src/overlay.tsx
+++ src/overlay.tsx
@@ -14,6 +14,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  * This project is derived from MagicBlackDecky (https://github.com/steam3d/MagicBlackDecky)
+ * which is copyright (c) libvibrant and licensed under GNU General Public License v3.0.
  */
 
 import { findModuleChild } from "decky-frontend-lib";
